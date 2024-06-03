79217424b6847c202a414511067505807902ec2d
User <davids710119@gmail.com>
2024-06-03 15:54:37 +0800

    交易紀錄、商品資料庫、個人帳戶資料庫

---
+++ app/src/main/java/com/example/mystore/products.java
@@ -0,0 +1,5 @@
+package com.example.mystore;
+
+public class products {
+  
+}
--- app/src/main/java/com/example/mystore/shoplist.java
+++ app/src/main/java/com/example/mystore/shoplist.java
@@ -9,6 +9,5 @@ public class shoplist extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shoplist);
-    }
+        setContentView(R.layout.activity_shoplist);    }
 }
